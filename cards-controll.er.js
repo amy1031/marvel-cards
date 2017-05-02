4f519e8b55c8ae90c103e70aabec0990c00a5ff0
Amy Roche <amy.roche31@gmail.com>
2017-05-01 21:20:45 -0600

    copied

---
+++ cards-controll.er.js
@@ -0,0 +1,85 @@
+function CardsController() {
+
+  // alias
+  var dataStore = new MarvelService()
+
+  // Ready is a local function. When you're done getting the characters, go get the ready function. 
+  // Passes a reference to that function. Whenever getCharacters is done
+  // doing what it needs to do, it will call the ready function
+  dataStore.getCharacters(ready)
+
+  this.onAdd = function (id) {
+    //this function will take the player that was clicked and add them to your team through the dataStore.
+    dataStore.addToPlayerCharacters(id);
+    draw(dataStore.getMarvelCharacters(), dataStore.getPlayerCharacters());
+    }
+
+  this.onRemove = function (id) {
+    //this will remove the character from your team
+    dataStore.removePlayerCharacter(id);
+    draw(dataStore.getMarvelCharacters(), dataStore.getPlayerCharacters());
+  }
+
+  this.createPlayer = function createPlayer(e) {
+    e.preventDefault();
+    var form = e.target;
+    dataStore.createPlayer(form.name.value)
+  //  draw(dataStore.getMarvelCharacters(), dataStore.getPlayerCharacters());
+  }
+
+  //calls draw and then what it received from getCharacters
+  function ready(data) {
+    draw(data, [])
+  }
+
+  function draw(marvelList, myList) {
+    //target is the id of the element where the list will be rendered
+    var marvelElem = document.getElementById('marvel-characters')
+    var myElem = document.getElementById('my-characters')
+    marvelElem.innerHTML = ''
+    myElem.innerHTML = ''
+
+    var marvelTemplate = ''
+    var playerTemplate1 = ''
+
+    // marvel list
+    for (var i in marvelList) {
+      var character = marvelList[i];
+      marvelTemplate += `
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">${character.name}</h3>
+        </div>
+        <div class="panel-body text-center">
+          <img src="${character.thumbnail.path}.${character.thumbnail.extension}" width="100" class="img-circle">
+        </div>
+        <div class="panel-footer text-center"><button class="btn-success" id="${character.id}" onclick="cardsCtrl.onAdd(${character.id})">Add to Team</button> </div>
+      </div>
+        `
+    }
+
+    // player list
+    for (var i in myList) {
+      var character = myList[i];
+      playerTemplate1 += `
+        <div class="panel panel-default text-center">
+          <div class="panel-heading">
+             <h3 class="panel-title">${character.name}</h3>
+          </div>
+          <div class="panel-body text-center">
+            <img src="${character.thumbnail.path}.${character.thumbnail.extension}" width="100" class="img-circle">
+          </div>
+          <div class="panel-footer text-center">
+            <button class="btn-danger" id="${character.id}" onclick="cardsCtrl.onRemove(${character.id})">Remove</button>
+          </div>
+      </div>
+          `
+    }
+
+    marvelElem.innerHTML = marvelTemplate
+    myElem.innerHTML = playerTemplate1;
+
+  }
+
+
+}
---
+++ marvel-service.js
@@ -0,0 +1,81 @@
+function MarvelService(){
+  var key = '?apikey=6cd5122c589aa703d8a1c249855727a7';
+  var baseUrl = 'http://gateway.marvel.com/v1/public/'
+  
+  var marvelCharacters = []; // controls everything that happens to our data
+  var myCharacters = [];
+  var playerDeck = [];
+
+  function Player(name){
+    var name = name;
+    this.playerDeck = [];
+  }
+
+  this.getPlayer = function(){
+      return name;
+    }
+  
+  this.getMarvelCharacters = function(){
+    //what should this function return
+    return marvelCharacters;
+  }
+  
+  // this.getMyCharacters = function(){
+  //   //what should this function return
+  //   return myCharacters;
+  // }
+
+  this.getPlayerCharacters = function(){
+    //what should this function return
+    return playerDeck;
+  }
+  
+  this.addToPlayerCharacters = function(id){
+    //in order to add a character to your list you will first need to find 
+    //the character by its id in the marvelCharacters array
+    for (var i = 0; i < marvelCharacters.length; i++) {
+      var marvelCharacter = marvelCharacters[i];
+      if (marvelCharacter.id == id & myCharacters.length < 6) {
+        marvelCharacters.splice(i, 1);
+        myCharacters.push(marvelCharacter);
+        i--;
+      }
+    }
+  }
+  
+  this.removePlayerCharacter = function(id){
+    //you need to find the character that you want to remove by its id
+    //and remove it.
+    for (var i = 0; i < myCharacters.length; i++) {
+      var myCharacter = myCharacters[i];
+      if (myCharacter.id == id) {
+        myCharacters.splice(i, 1)
+        marvelCharacters.push(myCharacter);
+        //marvelCharacters.sort();
+        i--;
+      }
+    }
+  }
+
+  this.createPlayer = function(name) {
+    var newPlayer = new Player(name)
+    playerDeck.push(newPlayer);
+  }
+  
+  // takes in an argument
+  this.getCharacters = function(callWhenDone){
+    var data = localStorage.getItem('MarvelData') // ignore for now
+    if(data){
+      marvelCharacters = JSON.parse(data);
+      return callWhenDone(marvelCharacters)
+    }
+    // $.get is a function built into jquery (callback)
+    $.get(baseUrl + 'characters'+key, function(response){ // says go to this url and get the data that is there
+      localStorage.setItem('MarvelData', JSON.stringify(response.data.results)) // when done, runs this function
+      marvelCharacters = response.data.results;
+      callWhenDone(marvelCharacters) // when callWhenDone is done, it will call the marvelCharacters function
+    })
+  }
+  
+  
+}
